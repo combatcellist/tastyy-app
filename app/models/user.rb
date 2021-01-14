4e29c4b382c8d913993938c35505057afccd3829
combatcellist <combatcellist913@gmail.com>
2021-01-14 15:04:37 +0900

    コミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+         validates :name, presence: true
+         validates :profile, presence: true
+         
+         has_many :items
+         has_many :comments
+       
 end
